7fb4afc7e83816068d4b1243e35c39c43a8e21d6
robin <robinl3680@gmail.com>
2018-06-27 17:23:04 +0530

    Administration

---
+++ src/views/Base/EntityType/CreateEntityType.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import {
+  Badge,
+  Button,
+  ButtonDropdown,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Col,
+  Collapse,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Fade,
+  Form,
+  FormGroup,
+  FormText,
+  FormFeedback,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Label,
+  Row,
+} from 'reactstrap';
+
+class Forms extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.toggleFade = this.toggleFade.bind(this);
+    this.state = {
+      collapse: true,
+      fadeIn: true,
+      timeout: 300
+    };
+  }
+
+  toggle() {
+    this.setState({ collapse: !this.state.collapse });
+  }
+
+  toggleFade() {
+    this.setState((prevState) => { return { fadeIn: !prevState }});
+  }
+
+  render() {
+    return (
+      <div className="animated fadeIn">
+        <Row>
+          <Col xs="12" sm="6">
+            <Card>
+              <CardHeader>
+                <strong>Entity Type</strong>
+                <small> Form</small>
+              </CardHeader>
+              <CardBody>
+                <Row>
+                  <Col xs="12">
+                    <FormGroup>
+                      <Label htmlFor="entity_type">Entity Type</Label>
+                      <Input type="text" id="entity_type" placeholder="Enter entity type" required />
+                      <br/>
+                      <Label htmlFor="entity_type_slug">Entity Type Slug</Label>
+                      <Input type="text" id="entity_type_slug" placeholder="Enter entity type slug" required />
+                      <div>
+                      <br/>
+                      <Input type="submit" id="submit" value="Submit" />
+                     
+                      </div>
+                    </FormGroup>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default Forms;
---
+++ src/views/Base/EntityType/ManageEntityType.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { Badge, Card, CardBody, CardHeader, Col, Pagination, PaginationItem, PaginationLink, Row, Table } from 'reactstrap';
+
+
+import {
+  Button,
+  ButtonDropdown,
+  Collapse,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Fade,
+  Form,
+  FormGroup,
+  FormText,
+  FormFeedback,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Label,
+} from 'reactstrap';
+
+
+class Tables extends Component {
+  render() {
+    return (
+      <div className="animated fadeIn">
+        <Row>
+          <Col xs="12" lg="6">
+            <Card>
+              <CardHeader>
+                <i className="fa fa-align-justify"></i> Delete Entity Type
+              </CardHeader>
+              <CardBody>
+                <Table responsive bordered>
+                  <thead>
+                  <tr>
+                    <th>Entity Type</th>
+                    <th>Entity Type Slug</th>
+                    <th>Option</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                    <td></td>
+                    <td></td>
+                    <td>
+                    <FormGroup>
+                      <Input type="submit" id="delete" value="Delete" />
+                      <Input type="submit" id="update" value="update" />
+                    </FormGroup>
+                    </td>
+                  </tr>
+                  </tbody>
+                </Table>
+                <Pagination>
+                  <PaginationItem><PaginationLink previous tag="button">Prev</PaginationLink></PaginationItem>
+                  <PaginationItem active>
+                    <PaginationLink tag="button">1</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem className="page-item"><PaginationLink tag="button">2</PaginationLink></PaginationItem>
+                  <PaginationItem><PaginationLink tag="button">3</PaginationLink></PaginationItem>
+                  <PaginationItem><PaginationLink tag="button">4</PaginationLink></PaginationItem>
+                  <PaginationItem><PaginationLink next tag="button">Next</PaginationLink></PaginationItem>
+                </Pagination>
+              </CardBody>
+            </Card>
+          </Col>
+
+        </Row>
+      </div>
+
+    );
+  }
+}
+
+export default Tables;
